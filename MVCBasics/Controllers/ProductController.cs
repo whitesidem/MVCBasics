3dcaa39d96e346f9635de35a6b37e48bfadfe187
Michael Whiteside <m.whiteside@blueyonder.co.uk>
2013-10-05 19:02:21 +0100

    push to github

---
+++ MVCBasics/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace MVCBasics.Controllers
+{
+    public class ProductController : Controller
+    {
+        public ActionResult List()
+        {
+            return View("Products");
+        }
+    }
+}
